8687ae45d23824e473ade18691a96028a53cc465
pradeep_k <kpradeep.eidiko@gmail.com>
2018-02-05 21:18:42 +0530

    Updated with Special_Chars_code

---
+++ Examples/Special_Chars_Replace/Replace/src/com/eidiko/Replace/ReplaceSpclChars.java
@@ -0,0 +1,40 @@
+package com.eidiko.Replace;
+
+public class ReplaceSpclChars {
+	
+
+	
+	public static String replaceChar(String ss)
+	{
+		String s = ss;
+		
+		String d[] = {"*","&","!","@","#","$","^","%","(",")","å","â","ã","ä","å","æ","ç","è","é","ê","ë","ì","í","î","ï","ð","ñ","ò","ó","ô","õ","ö","ø","ù","ú","û","ü","ý","þ","ÿ"};
+		String i[] = {"8","7","4","3","9","5","2","6","9","0","a","a","a","a","a","a","c","e","e","e","e","i","i","i","i","o","n","o","o","o","o","o","o","u","u","u","u","y","p","y"};
+	
+		int length = d.length;
+		System.out.println(length);
+		int j = 0;
+	
+		String newString = "";
+		while(j < length)
+		{
+			
+			
+			//Pattern p = Pattern.compile("[^a-z0-9 </>]", Pattern.CASE_INSENSITIVE);
+			//Matcher m = p.matcher(s);
+			String currChar = d[j];
+			String newValue = i[j];
+			if(s.contains(currChar))
+			{
+				 s = s.replace(currChar,newValue);
+				//System.out.println("replaced String is "+newString);
+			}
+			j = j + 1;
+			//s = newString;
+		}
+		
+		s=s.replace("4[CDATA", "![CDATA");
+	return s;
+	}
+
+}
